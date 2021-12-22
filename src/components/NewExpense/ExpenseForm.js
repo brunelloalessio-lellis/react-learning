0c45065bec3c5d8509b794fbee796d11ccb21fad
brunelloalessio-lellis <brunelloalessioandroid@gmail.com>
2021-12-22 12:08:23 +0100

    resetting data after submit with 2way data binding

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -50,6 +50,9 @@ const ExpenseForm = () => {
     }
 
     console.log(expenseData)
+    setEnteredAmount('')
+    setEnteredTitle('')
+    setEnteredDate('')
   };
 
   return (
@@ -57,7 +60,7 @@ const ExpenseForm = () => {
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input type="text" value={enteredTitle} onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
@@ -65,6 +68,7 @@ const ExpenseForm = () => {
             type="number"
             min="0.01"
             step="0.01"
+            value={enteredAmount}
             onChange={amountChangeHandler}
           />
         </div>
@@ -74,6 +78,7 @@ const ExpenseForm = () => {
             type="date"
             min="2019-01-01"
             max="2022-12-31"
+            value={enteredDate}
             onChange={dateChangeHandler}
           />
         </div>
