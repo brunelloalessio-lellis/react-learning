bd50bec01d2d40ed0c23e8f5c5dbb56e857711d4
brunelloalessio-lellis <brunelloalessioandroid@gmail.com>
2021-12-22 11:35:59 +0100

    form component creation

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,18 +4,17 @@ import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  useState(props.title);
+  const [title, setTitle] = useState(props.title);
 
   const clickHandler = () => {
-    console.log("clicked!");
-    props.title = "Updated title"
+    setTitle("Updated title")
   };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
