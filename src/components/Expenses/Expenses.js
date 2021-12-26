9eadc977bbced39190949b160654ed793770c89e
brunelloalessio-lellis <brunelloalessioandroid@gmail.com>
2021-12-26 09:08:53 +0100

    new expenses list component with if statement for renderer

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import Card from "../UI/Card";
-import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
   const [yearFilter, setYearFilter] = useState(`${new Date().getFullYear()}`);
@@ -15,20 +15,7 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === yearFilter;
   });
 
-  let expensesContent = <p>No expenses found.</p>;
-
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => {
-      return (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      );
-    });
-  }
+  
 
   return (
     <Card className="expenses">
@@ -36,7 +23,7 @@ const Expenses = (props) => {
         selected={yearFilter}
         onFilterChange={yearFilterChangeHandler}
       />
-      {expensesContent}
+      <ExpensesList items={filteredExpenses} />
     </Card>
   );
 };
